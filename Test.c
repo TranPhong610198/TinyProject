352f72454eaf2f0b3d09efcb6e325b410c25ce79
Moriarty <tphong610198@gmail.com>
2023-10-28 01:17:45 +0700

    Test push code

---
+++ Test.c
@@ -0,0 +1,11 @@
+// Test push code to github in this folder
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+#include<stdbool.h>
+int i, j;
+int main(){
+    printf("Hello World\n");
+    system("pause");
+}
